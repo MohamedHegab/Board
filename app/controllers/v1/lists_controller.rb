31962bd52f1b53fc332154288b4d2ee80510659c
a_bahaa <ahmedbahaa_11@hotmail.com>
2018-08-03 23:29:23 +0200

    version api

---
+++ app/controllers/v1/lists_controller.rb
@@ -0,0 +1,5 @@
+class V1::ListsController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :lists
+  end
+
   post 'signup', to: 'users#create'
   post 'auth/login', to: 'authentication#authenticate'
 
-  resources :lists
 end
